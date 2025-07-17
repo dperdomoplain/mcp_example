646b65803a50ee146b6184382c9b33376d487750
Daniel Perdomo Lorenzo <dperdomo1@almirall.com>
2025-07-17 12:35:48 +0200

    extract config

--- stack_overflow_mcp/tools/tools.py
+++ stack_overflow_mcp/tools/tools.py
@@ -5,6 +5,8 @@ from stack_overflow_mcp.sql_server.sql_server import (
     SQLServerConfigProvider,
 )
 
+config = SQLServerConfigProvider()
+
 
 def get_most_viewed_posts(numbers_of_posts: int) -> QueryResponse:
 
@@ -16,7 +18,7 @@ def get_most_viewed_posts(numbers_of_posts: int) -> QueryResponse:
         FROM [dbo].[Posts]
         ORDER BY [ViewCount] DESC
     """
-    config = SQLServerConfigProvider()
+
     sql_conn = SQLServerConnection(config)
     query_request = QueryRequest(query=query, params=[])
     result = sql_conn.execute_query(query_request)
@@ -33,7 +35,7 @@ def get_last_post_with_activity(numbers_of_posts: int) -> QueryResponse:
         FROM [dbo].[Posts]    
         ORDER BY [LastActivityDate] DESC
     """
-    config = SQLServerConfigProvider()
+
     sql_conn = SQLServerConnection(config)
     query_request = QueryRequest(query=query, params=[])
     result = sql_conn.execute_query(query_request)
